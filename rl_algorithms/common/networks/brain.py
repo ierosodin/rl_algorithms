afcda923b22a84e4d27129eb020449ab4a48191a
Leejin Jung <jin.prelude@gmail.com>
2020-05-06 10:25:45 +0900

    Add R2D1 DQNAgent

--- rl_algorithms/common/networks/brain.py
+++ rl_algorithms/common/networks/brain.py
@@ -54,3 +54,60 @@ class Brain(nn.Module):
         x = torch.zeros(state_dim).unsqueeze(0)
         output = self.backbone(x).detach().view(-1)
         return output.shape[0]
+
+
+class GRUBrain(Brain):
+    """Class for holding backbone and head networks."""
+
+    def __init__(
+        self, backbone_cfg: ConfigDict, head_cfg: ConfigDict,
+    ):
+        """Initialize."""
+        super(GRUBrain, self).__init__(backbone_cfg, head_cfg)
+        if not backbone_cfg:
+            self.backbone = identity
+            head_cfg.configs.input_size = head_cfg.configs.state_size[0]
+        else:
+            self.backbone = build_backbone(backbone_cfg)
+            head_cfg.configs.input_size = self.calculate_fc_input_size(
+                head_cfg.configs.state_size
+            )
+        self.gru = nn.GRU(
+            head_cfg.configs.input_size,
+            head_cfg.configs.rnn_hidden_size,
+            batch_first=True,
+        )
+        head_cfg.configs.input_size = head_cfg.configs.rnn_hidden_size
+        self.head = build_head(head_cfg)
+
+    def forward(self, x: torch.Tensor, hidden: torch.Tensor):
+        """Forward method implementation. Use in get_action method in agent."""
+        x = self.backbone(x)
+        if len(x.shape) == 1:
+            x = x.reshape(1, 1, -1)
+        hidden = torch.transpose(hidden, 0, 1)
+        x, hidden = self.gru(x, hidden)
+        x = self.head(x)
+
+        return x, hidden
+
+    def forward_(
+        self, x: torch.Tensor, hidden: torch.Tensor, n_tau_samples: int = None
+    ):
+        """Get output value for calculating loss."""
+        x = self.backbone(x)
+        if len(x.shape) == 1:
+            x = x.reshape(1, 1, -1)
+        hidden = torch.transpose(hidden, 0, 1)
+        x, hidden = self.gru(x, hidden)
+        if isinstance(self.head, IQNMLP):
+            x = self.head.forward_(x, n_tau_samples)
+        else:
+            x = self.head.forward_(x)
+        return x, hidden
+
+    def calculate_fc_input_size(self, state_dim: tuple):
+        """Calculate fc input size according to the shape of cnn."""
+        x = torch.zeros(state_dim).unsqueeze(0)
+        output = self.backbone(x).detach().view(-1)
+        return output.shape[0]
