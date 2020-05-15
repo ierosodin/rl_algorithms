c3ee6431b9cb93ebde61f58a86c39282de4d1b31
Leejin Jung <jin.prelude@gmail.com>
2020-05-15 09:29:43 +0900

    Implement rlpyt forward style

--- rl_algorithms/common/networks/brain.py
+++ rl_algorithms/common/networks/brain.py
@@ -81,50 +81,118 @@ class GRUBrain(Brain):
         self.head = build_head(head_cfg)
 
     def forward(self, x: torch.Tensor, hidden: torch.Tensor):
-        """Forward method implementation. Use in get_action method in agent."""
-        if isinstance(self.backbone, nn.Module):
-            if len(x.shape) > 4:
-                x = x.contiguous()
-                x_shape = x.shape
-                x = x.view(-1, x_shape[2], x_shape[3], x_shape[4])
-                x = self.backbone(x)
-                x = x.view(x_shape[0], x_shape[1], x.shape[-1])
+        def infer_leading_dims(tensor, dim):
+            """Looks for up to two leading dimensions in ``tensor``, before
+            the data dimensions, of which there are assumed to be ``dim`` number.
+            For use at beginning of model's ``forward()`` method, which should
+            finish with ``restore_leading_dims()`` (see that function for help.)
+            Returns:
+            lead_dim: int --number of leading dims found.
+            T: int --size of first leading dim, if two leading dims, o/w 1.
+            B: int --size of first leading dim if one, second leading dim if two, o/w 1.
+            shape: tensor shape after leading dims.
+            """
+            lead_dim = tensor.dim() - dim
+            assert lead_dim in (0, 1, 2)
+            if lead_dim == 2:
+                T, B = tensor.shape[:2]
             else:
-                x = self.backbone(x)
-                x = x.unsqueeze(0)
-        else:
-            if len(x.shape) == 1:
-                x = x.reshape(1, 1, -1)
-        hidden = torch.transpose(hidden, 0, 1)
-        x, hidden = self.gru(x, hidden)
-        x = self.head(x)
+                T = 1
+                B = 1 if lead_dim == 0 else tensor.shape[0]
+            shape = tensor.shape[lead_dim:]
+            return lead_dim, T, B, shape
 
-        return x, hidden
+        def restore_leading_dims(tensors, lead_dim, T=1, B=1):
+            """Reshapes ``tensors`` (one or `tuple`, `list`) to to have ``lead_dim``
+            leading dimensions, which will become [], [B], or [T,B].  Assumes input
+            tensors already have a leading Batch dimension, which might need to be
+            removed. (Typically the last layer of model will compute with leading
+            batch dimension.)  For use in model ``forward()`` method, so that output
+            dimensions match input dimensions, and the same model can be used for any
+            such case.  Use with outputs from ``infer_leading_dims()``."""
+            is_seq = isinstance(tensors, (tuple, list))
+            tensors = tensors if is_seq else (tensors,)
+            if lead_dim == 2:  # (Put T dim.)
+                tensors = tuple(t.view((T, B) + t.shape[1:]) for t in tensors)
+            if lead_dim == 0:  # (Remove B=1 dim.)
+                assert B == 1
+                tensors = tuple(t.squeeze(0) for t in tensors)
+            return tensors if is_seq else tensors[0]
+
+        img = x / 255.0
+        lead_dim, T, B, img_shape = infer_leading_dims(img, 3)
+
+        conv_out = self.backbone(img.view(T * B, *img_shape))  # Fold if T dimension.
+
+        lstm_input = conv_out.view(T, B, -1)
+        hidden = torch.transpose(hidden, 0, 1)
+        hidden = None if hidden is None else hidden
+        lstm_out, hidden = self.gru(lstm_input, hidden)
+
+        q = self.head(lstm_out.contiguous().view(T * B, -1))
+
+        # Restore leading dimensions: [T,B], [B], or [], as input.
+        q = restore_leading_dims(q, lead_dim, T, B)
+
+        return q, hidden
 
     def forward_(
         self, x: torch.Tensor, hidden: torch.Tensor, n_tau_samples: int = None
     ):
-        """Get output value for calculating loss."""
-        if isinstance(self.backbone, nn.Module):
-            if len(x.shape) > 4:
-                x = x.contiguous()
-                x_shape = x.shape
-                x = x.view(-1, x_shape[2], x_shape[3], x_shape[4])
-                x = self.backbone(x)
-                x = x.view(x_shape[0], x_shape[1], x.shape[-1])
+        def infer_leading_dims(tensor, dim):
+            """Looks for up to two leading dimensions in ``tensor``, before
+            the data dimensions, of which there are assumed to be ``dim`` number.
+            For use at beginning of model's ``forward()`` method, which should
+            finish with ``restore_leading_dims()`` (see that function for help.)
+            Returns:
+            lead_dim: int --number of leading dims found.
+            T: int --size of first leading dim, if two leading dims, o/w 1.
+            B: int --size of first leading dim if one, second leading dim if two, o/w 1.
+            shape: tensor shape after leading dims.
+            """
+            lead_dim = tensor.dim() - dim
+            assert lead_dim in (0, 1, 2)
+            if lead_dim == 2:
+                T, B = tensor.shape[:2]
             else:
-                x = self.backbone(x)
-                x = x.unsqueeze(0)
-        else:
-            if len(x.shape) == 1:
-                x = x.reshape(1, 1, -1)
+                T = 1
+                B = 1 if lead_dim == 0 else tensor.shape[0]
+            shape = tensor.shape[lead_dim:]
+            return lead_dim, T, B, shape
+
+        def restore_leading_dims(tensors, lead_dim, T=1, B=1):
+            """Reshapes ``tensors`` (one or `tuple`, `list`) to to have ``lead_dim``
+            leading dimensions, which will become [], [B], or [T,B].  Assumes input
+            tensors already have a leading Batch dimension, which might need to be
+            removed. (Typically the last layer of model will compute with leading
+            batch dimension.)  For use in model ``forward()`` method, so that output
+            dimensions match input dimensions, and the same model can be used for any
+            such case.  Use with outputs from ``infer_leading_dims()``."""
+            is_seq = isinstance(tensors, (tuple, list))
+            tensors = tensors if is_seq else (tensors,)
+            if lead_dim == 2:  # (Put T dim.)
+                tensors = tuple(t.view((T, B) + t.shape[1:]) for t in tensors)
+            if lead_dim == 0:  # (Remove B=1 dim.)
+                assert B == 1
+                tensors = tuple(t.squeeze(0) for t in tensors)
+            return tensors if is_seq else tensors[0]
+
+        img = x / 255.0
+        lead_dim, T, B, img_shape = infer_leading_dims(img, 3)
+
+        conv_out = self.backbone(img.view(T * B, *img_shape))  # Fold if T dimension.
+
+        lstm_input = conv_out.view(T, B, -1)
         hidden = torch.transpose(hidden, 0, 1)
-        x, hidden = self.gru(x, hidden)
-        if isinstance(self.head, IQNMLP):
-            x = self.head.forward_(x, n_tau_samples)
-        else:
-            x = self.head.forward_(x)
-        return x, hidden
+        hidden = None if hidden is None else hidden
+        lstm_out, hidden = self.gru(lstm_input, hidden)
+
+        q = self.head(lstm_out.view(T * B, -1))
+
+        # Restore leading dimensions: [T,B], [B], or [], as input.
+        q = restore_leading_dims(q, lead_dim, T, B)
+
+        return q, hidden
 
     def calculate_fc_input_size(self, state_dim: tuple):
         """Calculate fc input size according to the shape of cnn."""
